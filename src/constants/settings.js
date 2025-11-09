e2a064a36c2eb7b95a474ef9989ef2bc6c2144e5
pearlpandz <12746886+pearlpandz@users.noreply.github.com>
2025-11-09 10:02:15 +0530

    dev env config updated

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -1,4 +1,4 @@
 export const SETTINGS = {
-  DJANGO_URL: "https://admin.creavo.in",
-  FRAME_SERVICE_URL: "https://frame-service.creavo.in",
+  DJANGO_URL: "https://admin-dev.creavo.in",
+  FRAME_SERVICE_URL: "https://frame-service-dev.creavo.in",
 };
